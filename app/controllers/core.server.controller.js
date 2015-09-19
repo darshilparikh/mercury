babe562cbb4f3c43d4bbd2970fa2b048c1f90aec
oBigZee <zeeshan_m94@hotmail.com>
2015-09-19 02:21:10 -0400

    Indico HQ Sentiment API Call

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -9,3 +9,14 @@ exports.index = function(req, res) {
 		request: req
 	});
 };
+
+var indico = require('indico.io');
+indico.apiKey =  'ea5035782426051ad2b90811687b1ef0';
+
+var response = function(res) { console.log(res); }
+var logError = function(err) { console.log(err); }
+  
+// single example
+indico.sentimentHQ("kill yourself")
+  .then(response)
+  .catch(logError);
