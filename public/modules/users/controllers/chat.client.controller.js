8685ffc09b5d1ea8f0c7a01cde6150be90ef9b19
oBigZee <zeeshan_m94@hotmail.com>
2015-09-20 08:28:32 -0400

    e

--- public/modules/users/controllers/chat.client.controller.js
+++ public/modules/users/controllers/chat.client.controller.js
@@ -1,72 +1,5 @@
 'use strict';
 
-<<<<<<< HEAD
-angular.module('users').controller('ChatController', ['$scope','$http', 'Socket', 
-	function($scope, $http, Socket) {
-		
-		var bubbles = 1;
-        var maxBubbles = 8;
-        var server;
-        
-        var user = "Mike";
-        var customerid = "123";
-
-        $scope.avatar_urlSelf = '/modules/users/img/identicons/Unknown-2.png';
-        $scope.avatar_urlOther = '/modules/users/img/identicons/Unknown-3.png';
-        
-         $scope.sendMessage = function(msg) {
-             alert("Messege sent = " + msg);
-             var objToSend = 
-                {
-                    "name" : user,
-                    "id" : customerid,
-                    "text" : msg
-                }
-             
-            Socket.emit('mentorinit', {
-                data: objToSend
-                }, function (result) {
-                if (!result) {
-                        alert('There was an error changing your name');
-                } else {
-                
-                       alert(result);
-                }});
-         };
-        
-        
-        
-        function statCtrl($scope) {
-        
-                // the last received msg
-                $scope.msg = {};
-                
-                // handles the callback from the received event
-                var handleCallback = function (msg) {
-                        alert(JSON.stringify(msg));
-                        $scope.$apply(function () {
-                                $scope.msg = JSON.parse(msg.data)
-                        });
-                }
-                
-                var source = new EventSource('/supportkit/mentor/message');
-                source.addEventListener('message', handleCallback, false);
-        }
-        
-        //alert(io);
-        //alert(JSON.stringify(io));
-
-        Socket.on('connect', function(article) {
-                console.log(article);
-        });
-               
-               
-        
-        
-        
-
-        $scope.addBubble = function(avatar, text) {
-=======
 angular.module('users').controller('ChatController', ['$scope','$http', function($scope, $http) {
   var bubbles = 1;
   var maxBubbles = 8;
@@ -129,7 +62,6 @@ $scope.initMentor();
 
 
 $scope.addBubble = function(avatar, text) {
->>>>>>> half-second-refresh
 
             // Get the bytes of the image and convert it to a BASE64 encoded string and then
             // we use data URI to add dynamically the image data
